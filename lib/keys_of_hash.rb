dc9634b23106536c538ae94e45a0c178bd244a1c
sam ghoreishi <sam20gh@gmail.com>
2019-02-15 14:57:02 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,11 @@
 class Hash
   def keys_of(arguments)
-    # code goes here
+    animals = []
+		self.each { |animal, place|
+			if arguments.include?(place)
+				animals << animal
+			end
+		}
+		return animals
   end
 end
